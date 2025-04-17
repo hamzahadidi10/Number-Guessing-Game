f3fc40da29577a50800b77dac92153a0980dedb9
hamza <hadidihamza99@gmail.com>
2025-04-17 14:49:41 +0100

    100% OPTIMISATION

--- script.js
+++ script.js
@@ -2,33 +2,29 @@ const maxNum =100
 const minNum =1
 const result = Math.floor(Math.random()*(maxNum-minNum+1))
 
-console.log(result)
 let valid = true
 let attempts = 0;
-answer=window.prompt(`Choose a number between ${minNum} and ${maxNum}`)
+while(valid){
 
-if(isNaN(answer)){
-    window.alert("Enter a valid number")
     answer=window.prompt(`Choose a number between ${minNum} and ${maxNum}`)
-}
-else if(answer>maxNum || answer<minNum){
-    window.alert("Enter a valid number")
-    answer=window.prompt(`Choose a number between ${minNum} and ${maxNum}`)
-}
-while(valid)
-if(answer > result){
-    window.alert("TOO BIG try again")
-    answer=window.prompt(`Choose a number between ${minNum} and ${maxNum}`)
-    attempts++
-}
-else if(answer < result){
-    window.alert("TOO SMALL try again")
-    answer=window.prompt(`Choose a number between ${minNum} and ${maxNum}`)
-    attempts++
-}
-else if(answer == result){
-    window.alert(`CORRECT! Congratulations it took you ${attempts} attempts `)
-    attempts++
-    valid=false
-}
+    answer=Number(answer)
+
+    if(isNaN(answer)){
+        window.alert("Enter a valid number")
+    }
+    else if(answer>maxNum || answer<minNum){
+        window.alert("Enter a valid number")
+    }
+    else{
+        attempts++
+        if(answer > result){
+            window.alert("TOO BIG try again")
+    }
+    else if(answer < result){
+        window.alert("TOO SMALL try again")
+    }
+    else if(answer == result){
+        window.alert(`CORRECT! Congratulations it took you ${attempts} attempts `);
+        valid=false
+}}}
 
